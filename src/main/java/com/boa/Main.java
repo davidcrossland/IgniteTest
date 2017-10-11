b9d816a20ae0416ab954ad616dc1abd76933ea5c
davidcrossland <david@elastacloud.com>
2017-10-11 11:50:42 +0100

    blah

--- src/main/java/com/boa/Main.java
+++ src/main/java/com/boa/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static void main(String [] args)
     {
-        try (Ignite ignite = Ignition.start("example-ignite.xml")) {
+        try (Ignite ignite = Ignition.start("../example-ignite.xml")) {
             System.out.println();
             System.out.println("Compute runnable example started.");
 
